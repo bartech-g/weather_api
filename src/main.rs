244a08463c2373663c27fcb9e97321c387f7c129
bartech-g <baretch.js@gmail.com>
2024-10-20 08:49:54 +0000

    modify localhost

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(cors) // Wrap the app with CORS
             .service(get_weather)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
